fb316f43e098a6ce7eb0e0bdfa212a6a5ef1d453
Anthony Scinocco <scinocco.a@gmail.com>
2017-05-17 13:07:19 -0400

    final touches

--- Assignments/assn-1/code/Assn1-Boiling-AnthonyScinocco/main.cpp
+++ Assignments/assn-1/code/Assn1-Boiling-AnthonyScinocco/main.cpp
@@ -5,68 +5,79 @@ using namespace std;
 
 void takeInput()
 {
-    int choice = 8;
+    cout << "Enter '9' to exit program" << endl;
+    bool keepGoing = true;
 
-    while(choice != 9)
+    while(keepGoing)
     {
+        int choice;
         bool success = false;
         double boilingPoint;
 
         cout << "Celcius (0) \nFahrenheit (1) \nKelvin (2) \nSelect your unit of measure: " << endl;
         cin >> choice;
 
-        cout << "Enter boiling point: " << endl;
-        cin >> boilingPoint;
-
-        Water myWater;
-        Mercury myMercury;
-        Copper myCopper;
-        Silver mySilver;
-        Gold myGold;
-
-        cout << "-------------------" << endl;
-
-        if (myWater.isInRange(choice, boilingPoint))
+        if (choice == 9)
         {
-            cout << "Substance is water" << endl;
-            success = true;
-        }
-
-        if (myMercury.isInRange(choice, boilingPoint))
+            keepGoing = false;
+        } else
         {
-            cout << "Substance is Mercury" << endl;
-            success = true;
-        }
+            cout << "Enter boiling point: " << endl;
+            cin >> boilingPoint;
 
-        if (myCopper.isInRange(choice, boilingPoint))
-        {
-            cout << "Substance is copper" << endl;
-            success = true;
-        }
+            Water myWater;
+            Mercury myMercury;
+            Copper myCopper;
+            Silver mySilver;
+            Gold myGold;
 
-        if (mySilver.isInRange(choice, boilingPoint))
-        {
-            cout << "Substance is Silver" << endl;
-            success = true;
-        }
+            cout << "-------------------" << endl;
 
-        if (myGold.isInRange(choice, boilingPoint))
-        {
-            cout << "Substance is Gold" << endl;
-            success = true;
-        }
+            if (myWater.isInRange(choice, boilingPoint))
+            {
+                cout << "Substance is water" << endl;
+                success = true;
+            }
 
-        if (success == false)
-        {
-            cout << "Substance unknown..." << endl;
+            if (myMercury.isInRange(choice, boilingPoint))
+            {
+                cout << "Substance is Mercury" << endl;
+                success = true;
+            }
+
+            if (myCopper.isInRange(choice, boilingPoint))
+            {
+                cout << "Substance is copper" << endl;
+                success = true;
+            }
+
+            if (mySilver.isInRange(choice, boilingPoint))
+            {
+                cout << "Substance is Silver" << endl;
+                success = true;
+            }
+
+            if (myGold.isInRange(choice, boilingPoint))
+            {
+                cout << "Substance is Gold" << endl;
+                success = true;
+            }
+
+            if (success == false)
+            {
+                cout << "Substance unknown..." << endl;
+            }
+            
+            cout << "-------------------" << endl;
         }
-        
-        cout << "-------------------" << endl;
     }
 }
 
 int main()
 {
     takeInput();
+    cout << "-----------------" << endl;
+    cout << "Goodbye!" << endl;
+    cout << "-----------------" << endl;
     return 1;
 }

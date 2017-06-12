28f5c4b25c27bbe086ce1f6035571876fdb3898b
Anthony Scinocco <scinocco.a@gmail.com>
2017-06-12 17:35:07 -0400

    done with battery assignment, although im not sure how correct it is

--- Assignments/assn-2/code/Assn2-Battery-AnthonyScinocco/Models/Battery.hpp
+++ Assignments/assn-2/code/Assn2-Battery-AnthonyScinocco/Models/Battery.hpp
@@ -2,5 +2,47 @@ using namespace std;
 
 class Battery
 {
+    private:
+        double voltage; // V 
+        double joules; // J
+        double maxPower; // power in whats
+        double currentPower; // power in watts
+    public:
+        Battery(double max, double curr)
+        {
+            maxPower = max;
+            currentPower = curr;
+            voltage = 12;
+            joules = 5000000;
+        }
+        
+        bool powerDevice(double deviceAmps, double timeToBePoweredInSeconds)
+        {
+            // power required for device
+            double devicePowerReqd = deviceAmps * voltage;
+            double deviceJoulesReqd = devicePowerReqd * timeToBePoweredInSeconds;
 
+            if ((joules - deviceJoulesReqd) >= 0)
+            {
+                // update energy supply
+                joules = joules - deviceJoulesReqd;
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        // not sure what you meant, the wording was kind of confusing
+        void recharge()
+        {
+            currentPower = maxPower;
+            joules = 5000000;
+        }
+
+        double maxTime(double deviceAmps)
+        {
+            double P = deviceAmps * voltage;
+            double t = joules / P;
+            return t;
+        }
 };
--- Assignments/assn-2/code/Assn2-Battery-AnthonyScinocco/main.cpp
+++ Assignments/assn-2/code/Assn2-Battery-AnthonyScinocco/main.cpp
@@ -1,8 +1,24 @@
-#include "./loader.hpp";
+#include "./loader.hpp"
 
 using namespace std;
 
 int main()
 {
+    Battery myBattery(1000, 1000);
     
+    // power 4 amp light for 15 min
+    myBattery.powerDevice(4, 900); // 900s in 15 min
+    cout << "I just powered a lamp for 15min" << endl;
+
+    // how long can I run my 8 amp device
+    double mt = myBattery.maxTime(8);
+    cout << "I can power an 8 amp device for " + to_string(mt) + " seconds with my remaining energy" << endl;
+
+    myBattery.recharge();
+    cout << "I just recharged!" << endl;
+
+    mt = myBattery.maxTime(8);
+    cout << "I can power an 8 amp device for " + to_string(mt) + " seconds with a full charge" << endl;
+
+    return 0;
 }
